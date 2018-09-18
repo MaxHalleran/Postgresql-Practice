5dddf6f64d25f1782d06c0fc20f89d871c59db14
Max <MaxHalleran@gmail.com>
2018-09-18 18:37:11 +0000

    practiced modularity in project, enabled querying the database

--- pogre.js
+++ pogre.js
@@ -1,10 +1,22 @@
-const {Client} = require('pg');
-const client = new Client();
+const client = require('./client.js');
 
-client.connect();
+client.connect((err) => {
+  if (err) {
+    return console.error('Connection Error', err);
+  }
+  client.query('SELECT $1::int AS number', ['1'], (err, result) => {
+    if (err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].number);
+    console.log(result);
+    client.end();
+  });
+});
 
-const query(SELECT * FROM ${database}, [possible params]);
-
-const res = await client.query(query);
-console.log(res.rows[0].message);
-await client.end();
+// const query(SELECT * FROM ${database}, [possible params]);
+//
+// const res = await client.query(query);
+// console.log(res.rows[0].message);
+// await client.end();
+console.log(process.argv[2]);
